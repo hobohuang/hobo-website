08294bcd439c01ff3ed0ada21fb9668a06f3cd38
hobohuang <1536888730@qq.com>
2021-06-12 01:08:38 +0800

    添加element组件

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import installElementPlus from "./plugins/element";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.use(store).use(router).use(installElementPlus).mount("#app");
